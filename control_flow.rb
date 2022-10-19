cacc7190e19f81d95e816154f2f087c0a2e7d8d1
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-10-19 10:47:43 +0300

    Completed assignment

--- control_flow.rb
+++ control_flow.rb
@@ -2,11 +2,11 @@ def admin_login(username, password)
  if (username=="admin" || username == "ADMIN")&& password== "12345"
   "Access granted"
  else
-   puts "Access denied"
+  "Access denied"
  end
 end
 
-admin_login("name",12345)
+# admin_login("name",12345)
 
 def hows_the_weather(temperature)
   # your code here
@@ -15,19 +15,21 @@ def hows_the_weather(temperature)
  elsif temperature >=40 && temperature<=65
    "It's a little chilly out there!"
   elsif temperature >85
-    "it's too dang hot out there!"
+    "It's too dang hot out there!"
   else
     "It's perfect out there!"
  end
 end
 
-hows_the_weather (80)
+# hows_the_weather (80)
 
 def fizzbuzz(num)
   # your code here
- if num%3==0%5==0
+ if num%3==0 && num%5==0
    "FizzBuzz"
  elsif num%3==0
+  "Fizz" 
+ elsif num%5==0
   "Buzz" 
  else
   num
